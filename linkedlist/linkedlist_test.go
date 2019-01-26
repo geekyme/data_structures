ec5503e60d8928b821d41b5c687cb2b95eed1d3c
Shawn Lim <shawnlim89@live.com>
2019-01-26 17:59:20 +0800

    Add benchmark

--- linkedlist/linkedlist_test.go
+++ linkedlist/linkedlist_test.go
@@ -10,16 +10,37 @@ func TestAppend(t *testing.T) {
 	feed.Append(&post1)
 
 	if feed.start != &post1 || feed.end != &post1 {
-		t.Errorf("Did not append properly into an empty feed: %s", feed)
+		t.Errorf("Did not append properly into an empty feed: %v", feed)
 	}
 
 	feed.Append(&post2)
 
 	if feed.start != &post1 {
-		t.Errorf("Unexpectedly changed feed's first post: %s", feed)
+		t.Errorf("Unexpectedly changed feed's first post: %v", feed)
 	}
 
 	if feed.end != &post2 {
-		t.Errorf("Did not append post to the last: %s", feed)
+		t.Errorf("Did not append post to the last: %v", feed)
+	}
+}
+
+func BenchmarkAppend10000(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Append(10000)
+	}
+}
+
+func BenchmarkAppend100000(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Append(100000)
+	}
+}
+
+func Append(count int) {
+	feed := Feed{}
+
+	for n := 0; n < count; n++ {
+		post := Post{}
+		feed.Append(&post)
 	}
 }
